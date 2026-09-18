679b3fc55b8f8434b996696dc26a37610289c35d
Vijay Yadav <vijayyadav1002@gmail.com>
2026-09-18 09:10:31 -0500

    Add an installable PWA shell that caches the UI only

---
+++ rag-demo/static/sw.js
@@ -0,0 +1,40 @@
+const CACHE = "ask-northwind-v1";
+const SHELL = [
+  "/",
+  "/manifest.webmanifest",
+  "/sw.js",
+  "/icons/icon-192.png",
+  "/icons/icon-512.png",
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE).then((cache) => cache.addAll(SHELL)).then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.filter((k) => k !== CACHE).map((k) => caches.delete(k)))
+    ).then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  const url = new URL(event.request.url);
+  if (event.request.method !== "GET") return;
+  if (url.pathname === "/ws" || url.pathname.startsWith("/api/")) return;
+  event.respondWith(
+    caches.match(event.request).then((hit) => {
+      if (hit) return hit;
+      return fetch(event.request).then((res) => {
+        if (res.ok) {
+          const copy = res.clone();
+          caches.open(CACHE).then((cache) => cache.put(event.request, copy));
+        }
+        return res;
+      });
+    })
+  );
+});
